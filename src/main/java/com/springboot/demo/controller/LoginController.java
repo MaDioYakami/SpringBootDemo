e0c2e4e61ba4c0b6d6abd521968bb22d29c9d13c
madio <694140163@qq.com>
2018-12-04 23:13:24 +0800

    springmvc测试

--- src/main/java/com/springboot/demo/controller/LoginController.java
+++ src/main/java/com/springboot/demo/controller/LoginController.java
@@ -2,6 +2,9 @@ package com.springboot.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Demo class
@@ -23,5 +26,14 @@ public class LoginController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public ModelAndView login(@RequestParam("name") String name, @RequestParam("pwd") String pwd){
+        System.out.println(name);
+        System.out.println(pwd);
+        ModelAndView modelAndView = new ModelAndView("/home.html");
+        modelAndView.addObject("name",name);
+        return modelAndView;
     }
 }
