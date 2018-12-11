d206009ae643572d417122e64bcdf397a69f2d3b
madio <madioyakami@gmail.com>
2018-12-11 10:35:04 +0800

    新增批量测试，只需添加启动类注解和实现类注解就可以实现

---
+++ src/main/java/com/springboot/demo/Task/TestTask.java
@@ -0,0 +1,14 @@
+package com.springboot.demo.Task;
+
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestTask {
+
+    @Scheduled(cron = "*/5 * * * * ?")
+    public void testOneTask(){
+        System.out.println("测试客户！");
+    }
+}
